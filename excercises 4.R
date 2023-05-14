960b7cefefea73add1c8b9423350d41cd1bf215c
vsayki <130485326+vsayki@users.noreply.github.com>
2023-05-14 16:22:06 -0400

    Add files via upload

---
+++ excercises 4.R
@@ -0,0 +1,18 @@
+#4.1-4.8 general
+library("foreign")
+install.packages("psych")
+install.packages("devtools")
+library(devtools)
+install_github("djnavarro/TurtleGraphics")
+library(TurtleGraphics)
+turtle_init()
+#turtle image in plots!
+
+#excercises 4.9
+install.packages("lsr")
+library("lsr")
+install.packages("tidyverse")
+library("tidyverse")
+install.packages("car")
+library("car")
+#see general for github turtle and devtools and psych
